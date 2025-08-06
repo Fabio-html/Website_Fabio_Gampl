ad89dda3234261ba27308f68b97f9b1d4ca1a690
Fabio-html <fabiogampl@gmail.com>
2025-08-06 13:59:15 +0300

    only blog
    only blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,15 @@
 const { DateTime } = require("luxon");
 
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css')
-    eleventyConfig.addPassthroughCopy('./src/main.css')
-    eleventyConfig.addPassthroughCopy('./src/animation.js')
     eleventyConfig.addPassthroughCopy('./src/assets')
     eleventyConfig.addPassthroughCopy('./src/admin')
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-});
+    });
 
     return {
         dir: {
